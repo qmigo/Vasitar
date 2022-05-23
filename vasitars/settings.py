fe154d87c7b48e496ae890ff30dd4b472876df6f
qmigo <ankur031100@gmail.com>
2022-05-23 19:21:58 +0530

    about us added,and requirements.txt

--- vasitars/settings.py
+++ vasitars/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myApp'
+    'myApp',
+    'django-bootsrap-icons'
+
 ]
 
 MIDDLEWARE = [
